5bd043d31e614510e4093b627ec7e7e5931908f6
丁亚宾 <1262027459@qq.com>
2021-08-06 21:38:57 +0800

    first

---
+++ src/main/java/com/dingyabin/work/AnotherCatApplication.java
@@ -0,0 +1,17 @@
+package com.dingyabin.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author 丁亚宾
+ */
+@SpringBootApplication
+public class AnotherCatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnotherCatApplication.class, args);
+    }
+
+}
