17672b89e22729949a780f7cea2ee231622209a5
丁亚宾 <1262027459@qq.com>
2021-08-22 13:17:27 +0800

    重命名

---
+++ src/main/java/com/dingyabin/work/gui/component/ImageDisplayPanel.java
@@ -0,0 +1,28 @@
+package com.dingyabin.work.gui.component;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author 丁亚宾
+ * Date: 2021/8/22.
+ * Time:13:13
+ */
+public class ImageDisplayPanel extends JPanel {
+
+
+//    private
+
+
+
+
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage()
+    }
+
+
+
+
+}
