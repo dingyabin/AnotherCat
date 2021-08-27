e534b3e8b21a488f9fedeb096596876ae90cc3ac
丁亚宾 <1262027459@qq.com>
2021-08-27 23:22:27 +0800

    图片展示组件

---
+++ src/main/java/com/dingyabin/work/common/generator/CatCommentGenerator.java
@@ -0,0 +1,22 @@
+package com.dingyabin.work.common.generator;
+
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+/**
+ * @author 丁亚宾
+ * Date: 2021/8/27.
+ * Time:23:21
+ */
+public class CatCommentGenerator extends DefaultCommentGenerator {
+
+
+
+
+
+
+
+
+
+
+
+}
