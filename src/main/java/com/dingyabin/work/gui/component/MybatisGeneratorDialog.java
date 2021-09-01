4e579b2a838beea7f203e9eaaeff9f29429f04bb
丁亚宾 <1262027459@qq.com>
2021-09-02 00:09:37 +0800

    插件

--- src/main/java/com/dingyabin/work/gui/component/MybatisGeneratorDialog.java
+++ src/main/java/com/dingyabin/work/gui/component/MybatisGeneratorDialog.java
@@ -1,5 +1,6 @@
 package com.dingyabin.work.gui.component;
 
+import com.alee.extended.filechooser.WebDirectoryChooser;
 import com.alee.managers.style.StyleId;
 import com.dingyabin.work.gui.utils.GuiUtils;
 
@@ -7,22 +8,38 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * @author 丁亚宾
  * Date: 2021/8/29.
  * Time:22:15
  */
-public class MybatisGeneratorDialog extends JDialog {
+public class MybatisGeneratorDialog extends JDialog  implements ActionListener {
+
+    private LineBorder lineBorder = new LineBorder(CatColors.GENERATOR_WINDOW_BORDER, 3, true);
+
+    private JPanel projectInputPanel = new JPanel(new FlowLayout(FlowLayout.LEFT,30,5));
+
+    private JTextField projectInputField = GuiUtils.createTextField(60, StyleId.textfieldNoFocus);
+
+    private JButton projectInputBtn = GuiUtils.createButton("选择目录...", CatIcons.open, StyleId.button);
 
 
-    private JPanel projectInputPanel = new JPanel(new FlowLayout(FlowLayout.LEFT,10,5));
 
-    private JTextField projectInputField = GuiUtils.createTextField(50, StyleId.textfieldNoFocus);
+    private JPanel modelInputPanel = new JPanel();
 
-    private JButton projectInputBtn = GuiUtils.createButton("打开目录...", CatIcons.open, StyleId.button);
 
-    private LineBorder lineBorder = new LineBorder(CatColors.GENERATOR_WINDOW_BORDER, 4, true);
+    private JPanel daoInputPanel = new JPanel();
+
+
+    private JPanel sqlXmlInputPanel = new JPanel();
+
+
+
+
 
 
     public MybatisGeneratorDialog() {
@@ -32,13 +49,12 @@ public class MybatisGeneratorDialog extends JDialog {
 
 
 
-
     private void init(){
         generateComponent();
         //始终在最上面
         setAlwaysOnTop(true);
-        //大小设置
-        setSize(new Dimension(750, 850));
+        //自适应大小
+        pack();
         //可调大小
         setResizable(true);
         //位置居中设置
@@ -49,16 +65,37 @@ public class MybatisGeneratorDialog extends JDialog {
 
 
     private void generateComponent(){
-        //设置border
-        projectInputPanel.setBorder(new TitledBorder(lineBorder, "工程目录", TitledBorder.CENTER, TitledBorder.TOP, CatFonts.GENERATOR_WINDOW_BORDER));
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 
+        //工程路径选择
+        projectInputPanel.setBorder(new TitledBorder(lineBorder, "工程目录", TitledBorder.LEFT, TitledBorder.TOP, CatFonts.GENERATOR_WINDOW_BORDER));
         projectInputPanel.add(GuiUtils.createLabel("选择工程目录：", SwingConstants.RIGHT, 15));
         projectInputPanel.add(projectInputField);
+        projectInputBtn.addActionListener(this);
         projectInputPanel.add(projectInputBtn);
 
 
-        add(projectInputPanel);
+        //Model配置
+        modelInputPanel.setBorder(new TitledBorder(lineBorder, "Model&Bean配置", TitledBorder.LEFT, TitledBorder.TOP, CatFonts.GENERATOR_WINDOW_BORDER));
 
+        //DAO配置
+        daoInputPanel.setBorder(new TitledBorder(lineBorder, "DAO配置", TitledBorder.LEFT, TitledBorder.TOP, CatFonts.GENERATOR_WINDOW_BORDER));
+
+        //SQL XML配置
+        sqlXmlInputPanel.setBorder(new TitledBorder(lineBorder, "XMLMapper配置", TitledBorder.LEFT, TitledBorder.TOP, CatFonts.GENERATOR_WINDOW_BORDER));
+
+
+
+
+        contentPane.add(projectInputPanel);
+        contentPane.add(Box.createVerticalStrut(10));
+        contentPane.add(modelInputPanel);
+        contentPane.add(Box.createVerticalStrut(10));
+        contentPane.add(daoInputPanel);
+        contentPane.add(Box.createVerticalStrut(10));
+        contentPane.add(sqlXmlInputPanel);
+        setContentPane(GuiUtils.createJscrollPane(contentPane));
     }
 
 
@@ -67,6 +104,28 @@ public class MybatisGeneratorDialog extends JDialog {
     public void showSelf(){
         //弹出
         setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source == projectInputBtn) {
+            selectProjectPath();
+        }
+
+    }
+
+
+
+    /**
+     * 选择工程路径
+     */
+    private void selectProjectPath() {
+        File file = WebDirectoryChooser.showDialog(MybatisGeneratorDialog.this, "选择工程目录");
+        if (file != null) {
+            projectInputField.setText(file.getAbsolutePath());
+        }
     }
 
 
